422a818df263e08e89b66edd67f6fd6ef8ce50bd
FrancescoGalanti <galanti_francesco>
2020-10-20 14:32:00 +0200

    esercizio finito

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -8,8 +8,25 @@
 
 var arraynumber = [];
 
-// arraynumber = Math.floor(Math.random() * 100) + 1;
-//
-// console.log(arraynumber)
+// var number = Math.floor(Math.random() * 100) + 1;
 
-for()
+
+for(var i = 0; i <= 19; i++){
+    var number = Math.floor(Math.random() * 100) + 1;
+
+    if(number % 3 == 0){
+      arraynumber.push("Fizz");
+
+    }if(number % 5 == 0){
+          arraynumber.push("Buzz");
+        }
+     if((number % 5 == 0) && (number % 3)){
+          arraynumber.push("FizzBuzz");
+     }else{
+          arraynumber.push(number);
+     }
+
+
+}
+
+console.log(arraynumber)
