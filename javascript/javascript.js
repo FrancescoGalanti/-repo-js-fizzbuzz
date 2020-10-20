57f330c1bc7157ba7aa373c12651e7338c19d5f8
FrancescoGalanti <galanti_francesco>
2020-10-20 14:11:05 +0200

    inizio

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -4,47 +4,12 @@
 
 
 
-// // prima variabile //
-//  var email = ["email@francesco.it", "email@dario.it", "email@laura.it"];
-//
-//  // seconda variabile //
-//
-//  var emailuser = prompt("Inserisci la tua email").toLowerCase();
-//
-//  // controllo//
-//
-//  var userfound = false;
-//
-//
-//  for( var i = 0;  i < email.length; i++ ){
-//
-//     var item = email[i];
-//
-//     if( item === emailuser) {
-//         userfound = true;
-//     }
-//
-//  }
-//
-//  if(userfound == true) {
-//    console.log("Utente trovato, procedi con l\"accesso");
-//  } else(
-//     console.log("errore di autenticazione")
-//  )
+//  prima variabile //
 
-// // secondo esercizio //
+var arraynumber = [];
+
+// arraynumber = Math.floor(Math.random() * 100) + 1;
 //
-// // prima variabile //
-// var numerogiocatore = Math.floor((Math.random() * 6) + 1);
-//
-// // console.log(numerogiocatore) //
-//
-// var numerocomputer = Math.floor((Math.random() * 6) + 1);
-//
-// // output //
-//
-// if(numerogiocatore > numerocomputer) {
-//   console.log("IL GIOCATORE VINCE")
-// } else{
-//   console.log("IL COMPUTER VINCE")
-// }
+// console.log(arraynumber)
+
+for()
