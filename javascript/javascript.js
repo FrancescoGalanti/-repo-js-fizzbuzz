a889680d8b91d438a94b288bb6ea16d8618f7d84
FrancescoGalanti <galanti_francesco>
2020-10-20 16:59:36 +0200

    fatto vabene cosi ?

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -11,32 +11,34 @@ var arraynumber = [];
 // var numt = 5 / 3;
 // console.log(numt);
 
-var number = Math.floor(Math.random() * 100) + 1;
-console.log(number);
+// var number = Math.floor(Math.random() * 100) + 1;
+// console.log(number);
+//
+// var fritz = number / 3;
+//
+// var buzz = number / 5;
+//
+//
+//
+// console.log(fritz);
+//
+//
+// Number.isInteger(fritz);
+//
+// console.log(fritz)
+//
+//
+//  if((Number.isInteger(fritz) ) && (Number.isInteger(buzz) ) ){
+//    arraynumber.push("fritzbuzz");
+//  } else if(Number.isInteger(fritz) ) {
+//    arraynumber.push("fritz");
+//  } else if(Number.isInteger(buzz) ) {
+//    arraynumber.push("buzz")
+//  }
 
-var fritz = number / 3;
 
-var buzz = number / 5;
-
-
-
-console.log(fritz);
-
-
-Number.isInteger(fritz);
-
-console.log(fritz)
-
-
- if((Number.isInteger(fritz) ) && (Number.isInteger(buzz) ) ){
-   arraynumber.push("fritzbuzz");
- } else if(Number.isInteger(fritz) ) {
-   arraynumber.push("fritz");
- } else if(Number.isInteger(buzz) ) {
-   arraynumber.push("buzz")
- }
-
-
+// if((number % 3 === 0)){
+//   arraynumber.push("Fizz");
 
 
 
@@ -44,23 +46,21 @@ console.log(fritz)
 console.log(arraynumber);
 
 
-// for(var i = 0; i <= 19; i++){
-//     var number = Math.floor(Math.random() * 100) + 1;
-//
-//     if((number % 3) == (Number.isInteger(number)){
-//       arraynumber.push("Fizz");
-//
-//     }
-//     if((number % 5) == (isInteger(number)){
-//           arraynumber.push("Buzz");
-//         }
-//      if((number  % 5 == number.isInteger()) && (number % 3 == number.isInteger())){
-//           arraynumber.push("FizzBuzz");
-//      }else{
-//           arraynumber.push(number);
-//      }
-//
-//
-// }
-//
-// console.log(arraynumber)
+for(var i = 1; i <= 100; i++){
+    var number = Math.floor(Math.random() * 100) + 1;
+    if((number  % 5 === 0) && (number % 3 === 0)){
+          arraynumber.push("FizzBuzz");
+      } else if(number % 5 === 0){
+          arraynumber.push("Fizz");
+      } else if(number % 3 === 0){
+          arraynumber.push("buzz");
+      } else{
+          arraynumber.push(number);
+      }
+
+
+
+
+}
+
+console.log(arraynumber)
